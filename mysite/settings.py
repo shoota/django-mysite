b4071a32020e6c9138bfda8077c678148c93f7b0
Shoota Kumano <shoota.dev@gmail.com>
2017-06-19 22:41:28 +0900

    add ClubTeam model and migration

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sampleapp',
 ]
 
 MIDDLEWARE = [
@@ -78,9 +79,9 @@ DATABASES = {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '******',
-        'USER': '******',
-        'PASSWORD': '******',
+        'NAME': 'mysite_db',
+        'USER': 'mysite_user',
+        'PASSWORD': 'djangomysitedb',
         'HOST': 'localhost',
         'PORT': '3306',
     }
--- sampleapp/models.py
+++ sampleapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class ClubTeam(models.Model):
+	author = models.ForeignKey('auth.User')
+	name = models.CharField(max_length=100)
+	created_date = models.DateTimeField(default=timezone.now) 
+
+	def __str__(self):
+		return self.name
