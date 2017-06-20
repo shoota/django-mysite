92b6a03256d6660d176c675755d1cc3ea96839be
Shoota Kumano <shoota.dev@gmail.com>
2017-06-20 22:58:42 +0900

    add front url mapping and view
    to be able to access URL for sampleapp as front.

--- sampleapp/admin.py
+++ sampleapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ClubTeam
 
 # Register your models here.
+# admin.site.register(ClubTeamAdmin)
+
+class ClubTeamAdmin(admin.ModelAdmin):
+	list_display = ('name', 'location')
+admin.site.register(ClubTeam, ClubTeamAdmin)
--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def club_team_list(request):
+	return render(request, 'club_team/club_team_list.html', {})
