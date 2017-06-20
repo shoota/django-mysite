3f92df56b4821c19d157c63a086160f479272059
Shoota Kumano <shoota.dev@gmail.com>
2017-06-20 22:58:03 +0900

    expand ClubTeam entity

--- sampleapp/models.py
+++ sampleapp/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 class ClubTeam(models.Model):
 	author = models.ForeignKey('auth.User')
 	name = models.CharField(max_length=100)
+	location = models.CharField(max_length=100, null=True)
 	created_date = models.DateTimeField(default=timezone.now) 
 
 	def __str__(self):
