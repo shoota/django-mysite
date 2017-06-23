0b35388276a1b142021f9c72bb7fad3c4a75094e
Shoota Kumano <shoota.dev@gmail.com>
2017-06-23 16:01:26 +0900

    retrieved ClubTeam objects output to view

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import ClubTeam
 
 # Create your views here.
 
 def club_team_list(request):
-	return render(request, 'club_team/club_team_list.html', {})
+	clubs = ClubTeam.objects.all().order_by('created_date')
+	return render(request, 'club_team/club_team_list.html', {'clubs':clubs})
