7e42d1b8a31fde6c0a4f1db25e488fe6f90ce4bb
Shoota Kumano <shoota.dev@gmail.com>
2017-06-25 23:38:00 +0900

    add link to detail page from list with ClubTeam

--- sampleapp/urls.py
+++ sampleapp/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^clubs/', views.club_team_list),
+	url(r'^clubs/', views.club_teams),
+	url(r'^club/(?P<pk>[0-9]+)/$', views.club_detail, name='club_detail'),
 ]
--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ClubTeam
 
 # Create your views here.
 
-def club_team_list(request):
+def club_teams(request):
 	clubs = ClubTeam.objects.all().order_by('created_date')
 	return render(request, 'club_team/club_team_list.html', {'clubs':clubs})
+
+def club_detail(request, pk):
+	club = get_object_or_404(ClubTeam, pk=pk)
+	return render(request, 'club_team/club_detail.html', {'club':club})
